99fb417a5315b42ecedad5811d382549e990ffba
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-11-07 21:56:43 +0900

    add db/context & add mutation & update query

--- server-nexus/api/graphql/Post.ts
+++ server-nexus/api/graphql/Post.ts
@@ -1,4 +1,4 @@
-import { extendType, objectType } from "nexus"
+import { extendType, intArg, nonNull, objectType, stringArg } from "nexus"
 
 export const Post = objectType({
   name: 'Post',
@@ -15,9 +15,55 @@ export const PostQuery = extendType({
   definition(t) {
     t.nonNull.list.field('drafts', {
       type: 'Post',
-      resolve() {
-        return [{ id: 1, title: 'Nexus', body: '...', published: false }]
+      resolve(_root, _args, ctx) {
+        return ctx.db.posts.filter(p => p.published === false)
       },
+    }),
+    t.list.field('posts', {
+      type: 'Post',
+      resolve(_root, _args, ctx) {
+        return ctx.db.posts.filter(p => p.published === true)
+      }
+    })
+  }
+})
+
+export const PostMutation = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.nonNull.field('createDraft', {
+      type: 'Post',
+      args: {
+        title: nonNull(stringArg()),
+        body: nonNull(stringArg()),
+      },
+      resolve(_root, args, ctx) {
+        const draft = {
+          id: ctx.db.posts.length + 1,
+          title: args.title,
+          body: args.body,
+          published: false
+        }
+        ctx.db.posts.push(draft)
+        return draft
+      }
+    }),
+    t.field('published', {
+      type: 'Post',
+      args: {
+        draftId: nonNull(intArg())
+      },
+      resolve(_root, args, ctx) {
+        let draftToPublish = ctx.db.posts.find(p => p.id === args.draftId)
+
+        if (!draftToPublish) {
+          throw new Error('Could not find draft with id ' + args.draftId)
+        }
+
+        draftToPublish.published = true
+
+        return draftToPublish
+      }
     })
   }
 })
