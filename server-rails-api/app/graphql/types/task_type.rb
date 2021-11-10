f48e5f40799f18e9b0ea73456bbd042e7dc66deb
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-11-11 01:02:41 +0900

    regenerate graphql task type

--- server-rails-api/app/graphql/types/task_type.rb
+++ server-rails-api/app/graphql/types/task_type.rb
@@ -1,11 +1,12 @@
 module Types
   class TaskType < Types::BaseObject
-    description "task"
     field :id, ID, null: false
-    field :title, String, null: false, description: "タイトル"
-    field :description, String, null: false, description: "説明本文"
-    field :isFinished, Boolean, null: false, description: "完了済みか"
-    field :isDeleted, Boolean, null: false, description: "削除済みか"
-    field :version, Int, null: false, description: "バージョン"
+    field :title, String, null: true
+    field :description, String, null: true
+    field :isFinished, Boolean, null: true
+    field :isDeleted, Boolean, null: true
+    field :version, Integer, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
