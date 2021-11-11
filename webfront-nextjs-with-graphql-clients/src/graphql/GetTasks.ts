8a838d5c1993c8884fe011d63177bbd651d5949b
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-11-12 01:30:38 +0900

    add comments to challenge using relay

--- webfront-nextjs-with-graphql-clients/src/graphql/GetTasks.ts
+++ webfront-nextjs-with-graphql-clients/src/graphql/GetTasks.ts
@@ -1,5 +1,19 @@
 import { graphql } from 'react-relay'
 
+/**
+ * Not working...
+ * relay-compiler --src ./src/graphql --schema ./server_rails_api_schema.graphql --extensions ts
+ * 
+ * Writing js
+ * ERROR:
+ * Invalid AST Node: { kind: "Root", operation: "query", loc: { kind: "Source", start: 3, end: 351, source: [Source] }, metadata: null, name: "GetTasksQuery", argumentDefinitions: [[Object]], directives: [], selections: [[Object]], type: Query }.
+ * 
+ * refs
+ * https://github.com/relay-tools/relay-compiler-language-typescript/issues/151
+ * https://github.com/facebook/relay/issues/2904
+ * https://stackoverflow.com/questions/69876036/relay-compiler-12-graphql-16-invalid-ast-node
+ */
+
 const GetTasks = graphql`
   query GetTasksQuery($after: String) {
     tasks(first: 3, after: $after) {
