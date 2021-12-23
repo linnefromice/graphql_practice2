286f54cad10344ae1b25f46c25b47b27a597345d
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-12-23 11:52:05 +0900

    update instrumentation - pass value from before_query to after_query by instance valiable

--- server-rails-api/app/graphql/modules/query_instrumentation.rb
+++ server-rails-api/app/graphql/modules/query_instrumentation.rb
@@ -4,10 +4,12 @@ module Modules
 
     def before_query(query)
       Rails.logger.info("Query begin: #{Time.now.to_i}")
+      @request_log = "REQUEST_LOG"
     end
 
     def after_query(query)
       Rails.logger.info("Query end: #{Time.now.to_i}")
+      Rails.logger.info("request_log: #{@request_log}")
     end
   end
 end
