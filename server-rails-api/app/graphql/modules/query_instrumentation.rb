c54d9fe28227a195ec3b369e94f389cde8e453ae
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-12-28 16:34:22 +0900

    implement save access logs

--- server-rails-api/app/graphql/modules/query_instrumentation.rb
+++ server-rails-api/app/graphql/modules/query_instrumentation.rb
@@ -3,17 +3,18 @@ module Modules
     module_function
 
     def before_query(query)
-      @begin_time = Time.now.iso8601(3)
-      sleep 3.0
+      @current_access_log = AccessLog.create(
+        operationName: query.operation_name,
+        beginTime: Time.now.iso8601(3),
+        query: query.query_string
+      )
     end
 
     def after_query(query)
-      Rails.logger.info("begin_time: #{@begin_time}")
-      Rails.logger.info("end_time: #{Time.now.iso8601(3)}")
-      Rails.logger.info("operation_name: #{query.operation_name}")
-      Rails.logger.info("query: #{query.query_string}")
-      Rails.logger.info("result: #{query.result.to_json}")
-      # binding.pry
+      @current_access_log.update(
+        endTime: Time.now.iso8601(3),
+        result: query.result.to_json
+      )
     end
   end
 end
